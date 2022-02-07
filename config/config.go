96dfbd9f67e01e9283b8191e2253a3c082abc871
Andrew Minkin <minkin.andrew@gmail.com>
2022-02-08 01:48:40 +0600

    Added stub methods for gRPC/Rest server

--- config/config.go
+++ config/config.go
@@ -4,6 +4,8 @@ import "github.com/kelseyhightower/envconfig"
 
 type AppConfig struct {
 	Env         string `envconfig:"ENV" default:"development"`
+	GRPCPort    int    `envconfig:"GRPC_PORT" default:"8080"`
+	HTTPPort    int    `envconfig:"HTTP_PORT" default:"8081"`
 	SupabaseURL string `envconfig:"SUPABASE_URL"`
 	SupabaseKey string `envconfig:"SUPABASE_KEY"`
 }
---
+++ server/server.go
@@ -0,0 +1,78 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/gen1us2k/cloudnative_todo_list/config"
+	"github.com/gen1us2k/cloudnative_todo_list/database"
+	"github.com/gen1us2k/cloudnative_todo_list/database/supabase"
+	"github.com/gen1us2k/cloudnative_todo_list/grpc/v1/todolist"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Server struct {
+	db       database.Database
+	config   *config.AppConfig
+	errGroup *errgroup.Group
+}
+
+func NewServer(c *config.AppConfig) (*Server, error) {
+	db, err := supabase.NewSupabaseDatabase(c)
+	if err != nil {
+		return nil, err
+	}
+	return &Server{db: db, config: c, errGroup: new(errgroup.Group)}, nil
+}
+
+func (s *Server) CreateTodo(ctx context.Context, todo *todolist.Todo) (*todolist.Response, error) {
+	return &todolist.Response{}, nil
+
+}
+
+func (s *Server) ListTodos(ctx context.Context, e *emptypb.Empty) (*todolist.TodoListResponse, error) {
+	return &todolist.TodoListResponse{}, nil
+}
+
+func (s *Server) UpdateTodo(ctx context.Context, todo *todolist.Todo) (*todolist.Todo, error) {
+	return &todolist.Todo{}, nil
+}
+
+func (s *Server) DeleteTodo(ctx context.Context, todo *todolist.Todo) (*todolist.Response, error) {
+	return &todolist.Response{}, nil
+}
+func (s *Server) startHTTP() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := todolist.RegisterTodolistAPIServiceHandlerServer(ctx, mux, fmt.Sprintf("localhost:%d", s.config.Port), opts)
+	if err != nil {
+		return err
+	}
+	return http.ListenAndServe(s.config.HTTPPort, mux)
+}
+func (s *Server) startGRPC() error {
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", s.config.GRPCPort))
+	if err != nil {
+		return err
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	todolist.RegisterTodolistAPIServiceServer(grpcServer, s)
+	return grpcServer.Serve(lis)
+}
+func (s *Server) Start() {
+	s.errGroup.Go(s.startGRPC)
+	s.errGroup.Go(s.startHTTP)
+}
+func (s *Server) Wait() error {
+	return s.errGroup.Wait()
+}
